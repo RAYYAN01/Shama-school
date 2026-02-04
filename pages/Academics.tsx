3b789569f781b4192c58717834db9d1c63a46fca
Rayyan Zaniullabidin <73011816+RAYYAN01@users.noreply.github.com>
2026-02-04 12:19:35 +0530

    image

--- pages/Academics.tsx
+++ pages/Academics.tsx
@@ -26,7 +26,7 @@ const HolisticGrowth: React.FC = () => {
   {
     title: 'Physical Education',
     description: 'Regular yoga, PT sessions, team sports and annual sports days that promote fitness, teamwork, and discipline.',
-    image: 'public/images/class-3.jpeg'
+    image: '/images/class-3.jpeg'
   },
   {
     title: 'Performing Arts',
